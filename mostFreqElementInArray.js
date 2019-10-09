4af863834a2365d485a56828cfdf34b0155fa42d
William Chan <wchan1992@gmail.com>
2019-10-08 20:41:08 -0700

    Complete

--- mostFreqElementInArray.js
+++ mostFreqElementInArray.js
@@ -4,20 +4,20 @@
 // E: Assume only one result
 
 var mostFreqElementInArray = function(array) {
-  var map = {};
+  var count = {};
   var maxElement = array[0];
   var maxCount = 0;
 
   for (var i = 0; i < array.length; i++) {
-    if (array[i] in map) {
-      map[array[i]]++
+    if (array[i] in count) {
+      count[array[i]]++
     } else {
-      map[array[i]] = 1;
+      count[array[i]] = 1;
     }
 
-    if (map[array[i]] > maxCount) {
+    if (count[array[i]] > maxCount) {
       maxElement = array[i];
-      maxCount = map[array[i]];
+      maxCount = count[array[i]];
     }
   }
 

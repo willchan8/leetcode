4d90db572c23a587d5b889de66ae4fcdd321e327
William Chan <wchan1992@gmail.com>
2020-06-12 19:29:59 -0700

    Complete

---
+++ selectionSort.js
@@ -0,0 +1,26 @@
+// Similar to bubble sort, but instead of first placing larger values into sorted position, it places small values into the sorted position (beginning of array)
+
+// Store first element as the minValue
+// Compare the minValue to the next item in the array until you find a smaller value
+// If a smaller value is found, set the smaller number (and its index) to be the minimum and continue until the end of the array
+// If the minimum is not the value (index) you initially began with, swap the two values
+
+let selectionSort = (nums) => {
+  let minIndex;
+  for (let i = 0; i < nums.length; i++) {
+    minIndex = i;
+    minValue = nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < minValue) {
+        minIndex = j;
+        minValue = nums[j];
+      }
+    }
+    if (minIndex !== i) {
+      let temp = nums[i];
+      nums[i] = nums[minIndex];
+      nums[minIndex] = temp; 
+    }
+  }
+  return nums;
+}

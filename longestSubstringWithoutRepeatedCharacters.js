4a244530d13af4d797d123a9fc4fb0a90206707f
William Chan <wchan1992@gmail.com>
2020-06-16 16:44:11 -0700

    Complete

--- longestSubstringWithoutRepeatedCharacters.js
+++ longestSubstringWithoutRepeatedCharacters.js
@@ -3,38 +3,47 @@ var lengthOfLongestSubstring = function(s) {
   let hash = {};
   let left = 0;
   let right = 0;
-  let longest = 0;
+  let maxLength = 0;
   
   while (left < s.length && right < s.length) {
       let letter = s[right];
       if (!hash[letter]) {
           hash[letter] = 1;
           right++;
-          longest = Math.max(longest, right - left);
+          maxLength = Math.max(maxLength, right - left);
       } else {
           hash[s[left]] = 0;
           left++;
       }
   }
   
-  return longest;
+  return maxLength;
 };
 
 // Alternative solution
+/* 
+'abcbacd'
+   ^
+   When loop gets to s[3] (where s[3] = 'b'), the 'start' window would shift to s['b'] + 1 to where this 'c' is.
+   The hash then gets updated: 
+   hash = {'a' = 0, 'b' = 1, 'c' = 2} ---> hash = {'a' = 0, 'b' = 3, 'c' = 2}
+*/
+
+
 var findLongestSubstring = function(str) {
-  let seen = {};
-  let longest = 0;
+  let hash = {};
+  let maxLength = 0;
   let start = 0;
  
   for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (seen[char]) {
-      start = Math.max(start, seen[char]);
+    if (str[i] in hash && start <= hash[str[i]]) {
+      // start is the index of the left side of the window. See above for visual explanation
+      start = hash[str[i]] + 1;
+    } else {
+      maxLength = Math.max(maxLength, i - start + 1);
     }
-    // index - beginning of substring + 1 (to include current in count)
-    longest = Math.max(longest, i - start + 1);
-    // store the index of the next char so as to not double count
-    seen[char] = i + 1;
+    // save last seen index of each letter in the hash
+    hash[str[i]] = i;
   }
-  return longest;
+  return maxLength;
 }

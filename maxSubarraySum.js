99ba377fe8fcc77d6ce1c8f28dfe93e1e0e98269
William Chan <wchan1992@gmail.com>
2020-06-25 17:58:59 -0700

    Complete

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -5,16 +5,16 @@
 // Method: Sliding window approach.
 
 const maxSubArraySum = (arr) => {
-  let tempSum = arr[0];
+  let windowSum = arr[0];
   let maxSum = arr[0];
   
   for (let i = 1; i < arr.length; i++) {
       // Restart the window at the current value or expand the window to include the current value (whichever is larger)
-      // tempSum represents the sum of current sliding window
-      tempSum = Math.max(arr[i], tempSum + arr[i]);
+      // windowSum represents the sum of current sliding window
+      windowSum = Math.max(arr[i], windowSum + arr[i]);
       
       // Update and save the maxSum as needed
-      maxSum = Math.max(tempSum, maxSum);
+      maxSum = Math.max(windowSum, maxSum);
   }
   
   return maxSum;
@@ -22,18 +22,18 @@ const maxSubArraySum = (arr) => {
 
 // Same problem, but has an additional input num where num is the number of consecutive elements in the array (size of the subarray).
 const maxSubArraySum = (arr, num) => {
-  let tempSum = 0;
+  let windowSum = 0;
   let maxSum = 0;
 
-  // Set initial tempSum and maxSum to the first 'num' elements of the array
+  // Set initial windowSum and maxSum to the first 'num' elements of the array
   for (let i = 0; i < num; i++) {
-    tempSum += arr[i];
+    windowSum += arr[i];
     maxSum += arr[i];
   }
 
   for (let i = num; i < arr.length; i++) {
-    tempSum = arr[i] + tempSum - arr[i - num];
-    maxSum = Math.max(tempSum, maxSum);
+    windowSum = arr[i] + windowSum - arr[i - num];
+    maxSum = Math.max(windowSum, maxSum);
   }
 
   return maxSum;

4cd603b1e1d448aa172e51efe1a00f398ce68ecd
William Chan <wchan1992@gmail.com>
2020-06-28 14:31:15 -0700

    Complete

---
+++ stack.js
@@ -0,0 +1,44 @@
+// Implement Stack using Linked List
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    var node = new Node(val)
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      var temp = this.first;
+      this.first = node;
+      this.first.next = temp;
+    }
+    this.size++;
+    return this.size;
+  }
+
+  pop() {
+    if (!this.first) {
+      return null;
+    } else {
+      var temp = this.first;
+      if (this.first === this.last) {
+        this.last = null;
+      }
+      this.first = this.first.next;
+      this.size--;
+      return temp.val;
+    }
+  }
+}

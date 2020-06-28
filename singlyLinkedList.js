fffe799f3b1596ada22bfcdda26b8eb761db67c3
William Chan <wchan1992@gmail.com>
2020-06-27 22:17:25 -0700

    Complete push and pop methods

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -1,4 +1,51 @@
 // Singly Linked List implementation using ES6 Classes
 // Great for insertion and deletion since it doesn't involve re-indexing.
 
-class 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    var current = this.head;
+    var prev;
+    while(current.next) {
+      prev = current;
+      current = current.next;
+    }
+    // When current.next is null (i.e. current is the tail)
+    this.tail = prev;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+}

596fc85703898444ea56b0f481e8f8e22055afdd
William Chan <wchan1992@gmail.com>
2020-06-28 22:28:36 -0700

    Complete

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,17 +1,63 @@
-var searchBST = function(root, val) {
-  if (root === null) {
-      return null;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  if (val === root.val) {
-      return root;
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
   }
-  
-  if (val < root.val) {
-      return searchBST(root.left, val);
+
+  insert(value) {
+    var node = new Node(value);
+    if (!this.root) {
+      this.root = node;
+      return this;
+    } else {
+      var current = this.root;
+      while (current) {
+        if (value === current.value) {
+          return undefined;
+        }
+        if (value > current.value) {
+          if (!current.right) {
+            current.right = node;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+        if (value < current.value) {
+          if (!current.left) {
+            current.left = node;
+            return this;
+          } else {
+            current = current.left;
+          }
+        }
+      }
+    }
   }
-  
-  if (val > root.val) {
-      return searchBST(root.right, val);
+
+  find(value) {
+    if (!this.root) {
+      return;
+    }
+    var current = this.root;
+    while (current !== null) {
+      if (value === current.value) {
+        return true;
+      }
+      if (value > current.value) {
+        current = current.right;
+      } 
+      if (value < current.value) {
+        current = current.left;
+      }
+    }
+    return false;
   }
-};
+}

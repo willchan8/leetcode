0c383cb1c5db136baaecaa2b3fce01a3162e2ea1
William Chan <wchan1992@gmail.com>
2020-07-12 22:22:49 -0700

    Complete

--- maxDepthBinaryTree.js
+++ maxDepthBinaryTree.js
@@ -32,5 +32,7 @@ var maxDepth = function(root) {
     if (root === null) {
       return 0;
     }
-    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    let left = maxDepth(root.left);
+    let right = maxDepth(root.right);
+    return Math.max(left, right) + 1;
 };

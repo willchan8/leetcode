22a092e7bcfde5acb0a777f29980f2fcb4e3b026
William Chan <wchan1992@gmail.com>
2020-07-14 14:10:31 -0700

    Complete

---
+++ graph.js
@@ -0,0 +1,26 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+  }
+
+  removeVertex(vertex) {
+    this.adjacencyList[vertex].forEach(edge => this.removeEdge(vertex, edge));
+    delete this.adjacencyList[vertex];
+  }
+}
